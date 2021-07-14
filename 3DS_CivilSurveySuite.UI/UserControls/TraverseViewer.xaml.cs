4095fbd91c6cc4be06b91f15ef82d80aa082ae2a
Scott Whitney <scottwhits@gmail.com>
2021-07-14 17:03:23 +0930

    Basic Traverse Preview created for TraverseAngle

--- 3DS_CivilSurveySuite.UI/UserControls/TraverseViewer.xaml.cs
+++ 3DS_CivilSurveySuite.UI/UserControls/TraverseViewer.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -12,16 +11,11 @@ namespace _3DS_CivilSurveySuite.UI.UserControls
     public partial class TraverseViewer : Window
     {
         private IReadOnlyList<Model.Point> _points;
-        private const double XIncrement = 10;
-        private const double YIncrement = 10;
-        private Point _startPoint;
-        private Point _endPoint;
-        private TranslateTransform _transform;
 
-        public double XMin { get; set; } = -50;
-        public double YMin { get; set; } = -50;
-        public double XMax { get; set; } = 50;
-        public double YMax { get; set; } = 50;
+        private static double XMin => -50;
+        private static double YMin => -50;
+        private static double XMax => 50;
+        private static double YMax => 50;
 
         public TraverseViewer()
         {
@@ -38,30 +32,22 @@ namespace _3DS_CivilSurveySuite.UI.UserControls
         public void Draw(IReadOnlyList<Model.Point> points)
         {
             if (points == null)
+            {
                 return;
+            }
 
             _points = points;
             var polyline = new Polyline();
             polyline.Stroke = Brushes.Red;
-            polyline.StrokeThickness = 2;
+            polyline.StrokeThickness = 1;
             foreach (var point in points)
             {
-                Point newPoint = _transform == null ? new Point(XNormalise(point.X), YNormalise(point.Y)) : 
-                                                      new Point(XNormalise(point.X) + _transform.X, YNormalise(point.Y) + _transform.Y);
+                var newPoint = new Point(XNormalise(point.X), YNormalise(point.Y));
                 polyline.Points.Add(newPoint);
             }
 
             ViewerCanvas.Children.Clear();
             ViewerCanvas.Children.Add(polyline);
-
-            // Apply transform from a shift if there is one.
-            if (_transform != null)
-            {
-                foreach (Shape drawingObject in ViewerCanvas.Children)
-                {
-                    drawingObject.RenderTransform = _transform;
-                }
-            }
         }
 
         private double XNormalise(double x)
@@ -72,73 +58,6 @@ namespace _3DS_CivilSurveySuite.UI.UserControls
         private double YNormalise(double y)
         {
             return (ViewerCanvas.Height - (y - YMin) * ViewerCanvas.Height / (YMax - YMin));
-        }
-
-        private void ViewerCanvas_OnMouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            double dx = (e.Delta > 0) ? XIncrement : -XIncrement;
-            double dy = (e.Delta > 0) ? YIncrement : -YIncrement;
-            double x0 = XMin + (XMax - XMin) * dx / ViewerCanvas.Width;
-            double x1 = XMax - (XMax - XMin) * dx / ViewerCanvas.Width;
-            double y0 = YMin + (YMax - YMin) * dy / ViewerCanvas.Height;
-            double y1 = YMax - (YMax - YMin) * dy / ViewerCanvas.Height;
-
-            XMin = x0;
-            XMax = x1;
-            YMin = y0;
-            YMax = y1;
-
-            Draw(_points);
-        }
-
-        private void ViewerCanvas_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (!ViewerCanvas.IsMouseCaptured)
-            {
-                _startPoint = e.GetPosition(ViewerCanvas);
-                ViewerCanvas.CaptureMouse();
-            }
-        }
-
-        private void ViewerCanvas_OnMouseMove(object sender, MouseEventArgs e)
-        {
-            if (!ViewerCanvas.IsMouseCaptured) 
-                return;
-
-            _endPoint = e.GetPosition(ViewerCanvas);
-
-            if (_transform == null)
-            {
-                _transform = new TranslateTransform();
-            }
-
-            _transform.X = _endPoint.X - _startPoint.X;
-            _transform.Y = _endPoint.Y - _startPoint.Y;
-
-            foreach (Shape drawingObject in ViewerCanvas.Children)
-            {
-                drawingObject.RenderTransform = _transform;
-            }
-        }
-
-        //BUG: Position is updated, but when you click to drag, it returns to the previous position, leaving the mouse where you licked
-        // So it's like it's offset. Need to apply the transform shift to the actual points, and not just a render?
-
-        private void ViewerCanvas_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            _endPoint = e.GetPosition(ViewerCanvas);
-            //var dx = (XMax - XMin) * (_endPoint.X - _startPoint.X) / ViewerCanvas.Width;
-            //var dy = (YMax - YMin) * (_endPoint.Y - _startPoint.Y) / ViewerCanvas.Height;
-
-            //XMin += dx;
-            //XMax += dx;
-            //YMin += dy;
-            //YMax += dy;
-
-            ViewerCanvas.ReleaseMouseCapture();
-            ViewerCanvas.Cursor = Cursors.Arrow;
-
-            //Draw(_points);
         }
     }
 }
