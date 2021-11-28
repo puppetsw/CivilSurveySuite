60af69355079c17eabfffa793cafad1af4bf1e36
Scott Whitney <scottwhits@gmail.com>
2021-11-28 23:01:32 +1030

    Adding Open and Save file dialog services.

---
+++ 3DS_CivilSurveySuite.UI/Services/IOpenFileDialogService.cs
@@ -0,0 +1,12 @@
+﻿// Copyright Scott Whitney. All Rights Reserved.
+// Reproduction or transmission in whole or in part, any form or by any
+// means, electronic, mechanical or otherwise, is prohibited without the
+// prior written consent of the copyright owner.
+
+namespace _3DS_CivilSurveySuite.UI.Services
+{
+    public interface IOpenFileDialogService
+    {
+        bool? ShowDialog();
+    }
+}
