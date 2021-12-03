663d603189107027765c2e7cf8d9ef24584e4852
Scott Whitney <scottwhits@gmail.com>
2021-12-04 07:45:51 +1030

    Unit test for C3D dll added

---
+++ 3DS_CivilSurveySuiteAcadTests/CogoPointTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using _3DS_CivilSurveySuite.C3D2017;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.Civil.DatabaseServices;
+using NUnit.Framework;
+
+namespace _3DS_CivilSurveySuiteAcadTests
+{
+    [TestFixture, Apartment(ApartmentState.STA)]
+    public class CogoPointTests : TestBase
+    {
+        [Test]
+        public void Test_Create_CogoPoint()
+        {
+            CogoPoint result = null;
+
+            void TestAction(Database db, Transaction tr)
+            {
+                CogoPointUtils.CreatePoint(tr, Point3d.Origin);
+                result = CogoPointUtils.GetByPointNumber(tr, 1);
+            }
+
+            ExecuteTestActions(null, TestAction);
+
+            Assert.IsTrue(result != null);
+        }
+    }
+}
