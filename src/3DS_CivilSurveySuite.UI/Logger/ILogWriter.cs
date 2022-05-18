ee73f26fa3e1cb172b58d333efe24a18288efa50
Scott Whitney <79826944+puppetsw@users.noreply.github.com>
2022-05-18 11:14:14 +0930

    Added a logger service

---
+++ src/3DS_CivilSurveySuite.UI/Logger/ILogWriter.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace _3DS_CivilSurveySuite.UI.Logger
+{
+    public interface ILogWriter
+    {
+        Task WriteLineToLogAsync(string text);
+
+        void WriteLineToLog(string text);
+    }
+}
---
+++ src/3DS_CivilSurveySuite.UI/Logger/ILogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace _3DS_CivilSurveySuite.UI.Logger
+{
+    public interface ILogger
+    {
+        void Info(string error, [CallerMemberName] string caller = "");
+        void Info(string info, object obj, [CallerMemberName] string caller = "");
+        void Info(Exception ex, string error = "", [CallerMemberName] string caller = "");
+        void Warn(string error, [CallerMemberName] string caller = "");
+        void Warn(Exception ex, string error = "", [CallerMemberName] string caller = "");
+        void Error(string error, [CallerMemberName] string caller = "");
+        void Error(Exception ex, string error = "", [CallerMemberName] string caller = "");
+        void UnhandledError(Exception ex, string error = "", [CallerMemberName] string caller = "");
+    }
+}
