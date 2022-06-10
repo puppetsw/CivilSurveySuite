fcee4ba0d53e996f7391d0fceae65808bc026a2d
Scott Whitney <79826944+puppetsw@users.noreply.github.com>
2022-06-10 14:56:28 +0930

    Fixed possible bug with description cases.

--- src/3DS_CivilSurveySuite.C3D2017/Services/ConnectLineworkService.cs
+++ src/3DS_CivilSurveySuite.C3D2017/Services/ConnectLineworkService.cs
@@ -3,12 +3,14 @@
 // means, electronic, mechanical or otherwise, is prohibited without the
 // prior written consent of the copyright owner.
 
+using System;
 using System.Collections.Generic;
 using _3DS_CivilSurveySuite.ACAD2017;
 using _3DS_CivilSurveySuite.Shared.Models;
 using _3DS_CivilSurveySuite.Shared.Services.Interfaces;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Geometry;
+using Autodesk.Civil;
 using Autodesk.Civil.DatabaseServices;
 
 namespace _3DS_CivilSurveySuite.C3D2017.Services
@@ -17,6 +19,7 @@ namespace _3DS_CivilSurveySuite.C3D2017.Services
     {
         public string DescriptionKeyFile { get; set; }
 
+        // ReSharper disable once CognitiveComplexity
         public void ConnectCogoPoints(IReadOnlyList<DescriptionKey> descriptionKeys)
         {
             using (Transaction tr = AcadApp.StartLockedTransaction())
@@ -25,13 +28,12 @@ namespace _3DS_CivilSurveySuite.C3D2017.Services
 
                 foreach (ObjectId pointId in C3DApp.ActiveDocument.CogoPoints)
                 {
-                    //BUG: Seems like there could be a problem with the keys and stuff if they don't match up
-                    //TODO: add way to check for special codes e.g. SL or RECT
-                    CogoPoint cogoPoint = pointId.GetObject(OpenMode.ForRead) as CogoPoint;
-                    /* in each DescriptionKeyMatch add a collection of join-able points separated by line number.
-                       using dictionaries with the key being the raw description and then the line number.?
-                     */
+                    // FIXED?: Seems like there could be a problem with the keys and stuff if they don't match up
+                    // 10/6/22 I wish I was more descriptive with this issue.
+                    // 10/6/22 Added upper invariant calls, hopefully solves this issue If I'm understanding what I wrote correctly.
 
+                    // TODO: add way to check for special codes e.g. SL or RECT
+                    var cogoPoint = pointId.GetObject(OpenMode.ForRead) as CogoPoint;
                     if (cogoPoint == null)
                     {
                         continue;
@@ -39,24 +41,26 @@ namespace _3DS_CivilSurveySuite.C3D2017.Services
 
                     foreach (DescriptionKey descriptionKey in descriptionKeys)
                     {
-                        if (DescriptionKeyMatch.IsMatch(cogoPoint.RawDescription, descriptionKey))
+                        if (!DescriptionKeyMatch.IsMatch(cogoPoint.RawDescription, descriptionKey, AcadApp.Logger))
                         {
-                            string description = DescriptionKeyMatch.Description(cogoPoint.RawDescription, descriptionKey);
-                            string lineNumber = DescriptionKeyMatch.LineNumber(cogoPoint.RawDescription, descriptionKey);
-
-                            DescriptionKeyMatch deskeyMatch;
-                            if (desMapping.ContainsKey(description))
-                            {
-                                deskeyMatch = desMapping[description];
-                            }
-                            else
-                            {
-                                deskeyMatch = new DescriptionKeyMatch(descriptionKey);
-                                desMapping.Add(description, deskeyMatch);
-                            }
-
-                            deskeyMatch.AddCogoPoint(cogoPoint.ToCivilPoint(), lineNumber);
+                            continue;
                         }
+
+                        string description = DescriptionKeyMatch.Description(cogoPoint.RawDescription, descriptionKey);
+                        string lineNumber = DescriptionKeyMatch.LineNumber(cogoPoint.RawDescription, descriptionKey);
+
+                        DescriptionKeyMatch deskeyMatch;
+                        if (desMapping.ContainsKey(description))
+                        {
+                            deskeyMatch = desMapping[description];
+                        }
+                        else
+                        {
+                            deskeyMatch = new DescriptionKeyMatch(descriptionKey);
+                            desMapping.Add(description, deskeyMatch);
+                        }
+
+                        deskeyMatch.AddCogoPoint(cogoPoint.ToCivilPoint(), lineNumber);
                     }
                 }
 
@@ -68,12 +72,15 @@ namespace _3DS_CivilSurveySuite.C3D2017.Services
                 {
                     DescriptionKeyMatch deskeyMatch = desKey.Value;
 
+                    AcadApp.Logger.Info($"Joining {deskeyMatch.DescriptionKey.Key}");
+
                     foreach (var joinablePoints in deskeyMatch.JoinablePoints)
                     {
                         Point3dCollection points = new Point3dCollection();
                         foreach (CivilPoint point in joinablePoints.Value)
                         {
                             points.Add(new Point3d(point.Easting, point.Northing, point.Elevation));
+                            AcadApp.Logger.Info($"Connectable point: x:{point.Easting}, y:{point.Northing} DesKey: {point.RawDescription}");
                         }
 
                         string layerName = deskeyMatch.DescriptionKey.Layer;
@@ -81,6 +88,7 @@ namespace _3DS_CivilSurveySuite.C3D2017.Services
                         //Check if the layer exists, if not create it.
                         if (!LayerUtils.HasLayer(layerName, tr))
                         {
+                            AcadApp.Logger.Info($"Creating layer: {layerName}");
                             LayerUtils.CreateLayer(layerName, tr);
                         }
 
@@ -94,6 +102,8 @@ namespace _3DS_CivilSurveySuite.C3D2017.Services
                             PolylineUtils.DrawPolyline3d(tr, btr, points, layerName);
                         }
                     }
+
+                    AcadApp.Logger.Info($"Completed joining {deskeyMatch.DescriptionKey.Key}");
                 }
 
                 tr.Commit();
