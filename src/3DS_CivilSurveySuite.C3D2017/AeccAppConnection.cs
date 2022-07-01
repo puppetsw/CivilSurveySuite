c5a6c2fd865f9d9f03e978596abc729ac22e6c32
Scott Whitney <79826944+puppetsw@users.noreply.github.com>
2022-07-01 12:21:02 +0930

    Fixed issue with site erase

---
+++ src/3DS_CivilSurveySuite.C3D2017/AeccAppConnection.cs
@@ -0,0 +1,35 @@
+﻿namespace _3DS_CivilSurveySuite.C3D2017
+{
+    public sealed class AeccAppConnection
+    {
+        private dynamic _aeccApp;
+
+        public dynamic AeccApp
+        {
+            get
+            {
+                if (_aeccApp == null)
+                {
+                    _aeccApp = AeccAppTools.GetAeccApp("Land");
+                }
+                return _aeccApp;
+            }
+        }
+
+        public dynamic AeccDoc
+        {
+            get
+            {
+                return AeccApp.ActiveDocument;
+            }
+        }
+
+        public dynamic AeccDb
+        {
+            get
+            {
+                return AeccApp.ActiveDocument.Database;
+            }
+        }
+    }
+}
