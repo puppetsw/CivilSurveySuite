3440a829393505364d3c7d0f8ec0f78ddd599987
Scott Whitney <79826944+puppetsw@users.noreply.github.com>
2022-07-03 22:38:16 +0930

    Flatten feature line command

---
+++ src/3DS_CivilSurveySuite.CIVIL/Commands/FlattenFeatureLineCommand.cs
@@ -0,0 +1,33 @@
+﻿using _3DS_CivilSurveySuite.ACAD;
+using _3DS_CivilSurveySuite.Shared.Services.Interfaces;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.Civil.DatabaseServices;
+
+namespace _3DS_CivilSurveySuite.CIVIL
+{
+    public class FlattenFeatureLineCommand : IAcadCommand
+    {
+        public void Execute()
+        {
+            var featureLines = new ObjectIdCollection();
+            if (!EditorUtils.TryGetImpliedSelectionOfType<FeatureLine>(out featureLines))
+            {
+                if (!EditorUtils.TryGetSelectionOfType<FeatureLine>("\n3DS> Select Feature Line: ", "\n3DS> Remove Feature Line: ", out featureLines))
+                {
+                    return;
+                }
+            }
+
+            using (var tr = AcadApp.StartTransaction())
+            {
+                foreach (ObjectId featureLineId in featureLines)
+                {
+                    var featureLine = (FeatureLine)tr.GetObject(featureLineId, OpenMode.ForWrite);
+                    FeatureLineUtils.FlattenFeatureLine(featureLine);
+                }
+
+                tr.Commit();
+            }
+        }
+    }
+}
