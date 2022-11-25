b9f7fd23051add7c5f3c721a95331d29932d6dfc
Scott Whitney <79826944+puppetsw@users.noreply.github.com>
2022-11-25 11:32:05 +1030

    Moved DrawChainage to TransientGraphics class.

--- src/3DS_CivilSurveySuite.ACAD/TransientGraphics.cs
+++ src/3DS_CivilSurveySuite.ACAD/TransientGraphics.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using _3DS_CivilSurveySuite.Shared.Helpers;
 using _3DS_CivilSurveySuite.Shared.Models;
@@ -27,6 +28,7 @@ namespace _3DS_CivilSurveySuite.ACAD
     /// try
     /// {
     ///   graphics.DrawLine(point1, point2);
+    ///   graphics.Undo(); // If you need to undo last drawn.
     /// }
     /// finally
     /// {
@@ -79,19 +81,6 @@ namespace _3DS_CivilSurveySuite.ACAD
 
             foreach (Drawable entity in drawable)
                 TransientManager.CurrentTransientManager.AddTransient(entity, mode, 0, new IntegerCollection());
-        }
-
-        private void RemoveTransientDrawable(TransientDrawable drawable)
-        {
-            if (_drawables.Contains(drawable))
-            {
-                var d = _drawables[_drawables.IndexOf(drawable)];
-
-                for (int i = 0; i < d.Count - 1; i++)
-                    TransientManager.CurrentTransientManager.EraseTransient(d[i], new IntegerCollection());
-
-                _drawables.Remove(d);
-            }
         }
 
         public void DrawLines(IReadOnlyList<Point2d> coordinates, TransientDrawingMode mode = TransientDrawingMode.Main) => DrawLines(coordinates.ToListOfPoint3d(), mode);
@@ -431,6 +420,63 @@ namespace _3DS_CivilSurveySuite.ACAD
         public void Dispose()
         {
             Dispose(true);
+        }
+
+        /// <summary>
+        /// Draws a chainage between two <see cref="Point3d"/> entities.
+        /// </summary>
+        /// <param name="graphics">The <see cref="TransientGraphics"/> object to draw the chainage.</param>
+        /// <param name="firstPoint"></param>
+        /// <param name="secondPoint"></param>
+        /// <param name="interval"></param>
+        /// <param name="textSize"></param>
+        public void DrawChainage(Point3d firstPoint, Point3d secondPoint, int interval = 10, double textSize = 1.0)
+        {
+            var distance = PointHelpers.GetDistanceBetweenPoints(firstPoint.ToPoint(), secondPoint.ToPoint());
+            var angle = AngleHelpers.GetAngleBetweenPoints(firstPoint.ToPoint(), secondPoint.ToPoint());
+
+            var drawables = new TransientDrawable();
+
+            var intervalStep = 0;
+            while (intervalStep < distance)
+            {
+                var point = PointHelpers.AngleAndDistanceToPoint(angle, intervalStep, firstPoint.ToPoint());
+
+                drawables.Add(DrawTick(point.ToPoint3d(), angle));
+                drawables.Add(DrawChainageText(point.ToPoint3d(), intervalStep.ToString(), textSize, angle, planReadability: false));
+                intervalStep += interval;
+            }
+            //draw last
+            drawables.Add(DrawTick(secondPoint, angle));
+            drawables.Add(DrawChainageText(secondPoint, Math.Round(distance, 3).ToString(CultureInfo.InvariantCulture), textSize, angle, planReadability: false));
+            AddTransientDrawable(drawables);
+        }
+
+        private Drawable DrawChainageText(Point3d position, string text, double textSize, Angle angle, double offsetAmount = 0.5, bool planReadability = true)
+        {
+            var mText = new MText();
+            mText.SetDatabaseDefaults();
+
+            mText.Rotation = planReadability ?
+                angle.GetOrdinaryAngle().ToCounterClockwise().ToRadians() :
+                angle.ToCounterClockwise().ToRadians();
+
+            Point insPoint = PointHelpers.AngleAndDistanceToPoint(angle.GetOrdinaryAngle() - 90, TextOffset(offsetAmount), position.ToPoint());
+
+            mText.Location = insPoint.ToPoint3d();
+            mText.Color = Color;
+            mText.Attachment = AttachmentPoint.BottomCenter;
+            mText.TextHeight = TextSize(textSize);
+            mText.Contents = text;
+
+            return mText;
+        }
+
+        private Drawable DrawTick(Point3d tickPoint, Angle drawAngle, double tickLength = 1.0)
+        {
+            var point1 = PointHelpers.AngleAndDistanceToPoint(drawAngle + 90, tickLength, tickPoint.ToPoint());
+            var point2 = PointHelpers.AngleAndDistanceToPoint(drawAngle - 90, tickLength, tickPoint.ToPoint());
+            return DrawLine(point1.ToPoint3d(), point2.ToPoint3d(), useDashedLine: false);
         }
     }
 }
